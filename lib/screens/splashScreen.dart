44d8aeca50d5a3de81eb2b0ce1f6596c4da48f52
izzat <izzatmarzuqi88@gmail.com>
2021-01-16 18:29:09 +0800

    Set splash screen duration

--- lib/screens/splashScreen.dart
+++ lib/screens/splashScreen.dart
@@ -1,12 +1,12 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:ka_ching/models/profile_data.dart';
 import 'package:ka_ching/screens/dashboard.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
+      duration: 2500,
       splash: Container(
           child: Image(
         height: 800,
